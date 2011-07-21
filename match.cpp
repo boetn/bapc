fc5e39587ed5b2b030f34776aef4b38a95904d53
Jasper Desmadryl <jasper_desmadryl@hotmail.com>
2011-07-21 16:50:45 +0200

    Faster version using dynamic programming

--- match.cpp
+++ match.cpp
@@ -2,12 +2,10 @@
 #include <string>
 using namespace std;
 
-#define MAX 20 //should be 100
+#define MAX 100
 
 //match[i] contains number of matches needed to form i
 int match[] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
-string mi[MAX+1], ma[MAX+1];
-char curr[MAX+1] = "";
 
 //compare numbers saved as strings
 int mycmp(string s1, string s2)
@@ -20,39 +18,35 @@ int mycmp(string s1, string s2)
         return s1.compare(s2);
 }
 
-//i is the current digit, used are the number of matchsticks so far
-void fill(int i, int used)
-{
-    //check for smaller and bigger values
-    if (mycmp(curr, mi[used]) < 0) mi[used] = curr;
-    if (mycmp(curr, ma[used]) > 0) ma[used] = curr;
-
-    //recurse further, don't start numbers with a zero
-    for (int a = i ? '0' : '1'; a <= '9'; a++) {
-        int next = used + match[a-'0'];
-        if (next <= MAX) {
-            curr[i] = a;
-            curr[i+1] = '\0';
-            fill(i + 1, next);
-        }
-    }
-}
-
 int main()
 {
-    //initialise 
-    for (int i = 0; i <= MAX; i++) {
-        mi[i] = string(MAX, '9');
-        ma[i] = "0";
+    string mi[MAX+1], ma[MAX+1];
+    mi[0] = ma[0] = "";
+    ma[1] = string(MAX, '9');
+    for (int i = 1; i <= MAX; i++) {
+        mi[i] = string(MAX, '9'); // ~infinity
+        ma[i] = "";
     }
 
-    //precalculate the values
-    fill(0, 0);
+    //use previous answers to find an optimal one with i matchsticks
+    for (int i = 2; i <= MAX; i++) { // matchsticks
+        for (int j = 0; j < 10; j++) { // numbers
+            int prev = i - match[j];
+            if (prev < 0 || (!prev && !j))
+                continue;
+            //minimum
+            string cand = mi[prev] + char(j+'0');
+            if (mycmp(cand, mi[i]) < 0)
+                mi[i] = cand;
+            //maximum
+            cand = ma[prev] + char(j+'0');
+            if (mycmp(cand, ma[i]) > 0)
+                ma[i] = cand;
+        }
+    }
 
     int n;
     cin >> n;
-
-    //print those that were asked
     while (n--) {
         int num;
         cin >> num;
