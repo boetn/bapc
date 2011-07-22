02ba196f984a558150eca72af5469a757729485c
Jasper Desmadryl <jasper_desmadryl@hotmail.com>
2011-07-22 13:08:43 +0200

    Eerste versie van de template

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    return 0;
+}
