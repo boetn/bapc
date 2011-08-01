5e77b422891f38799468d5aec73a562ff6a644ac
Jasper Desmadryl <jasper_desmadryl@hotmail.com>
2011-08-01 20:14:35 +0200

    correcte versie van vito's family

--- prob002.cpp
+++ prob002.cpp
@@ -1,43 +1,77 @@
-#include <iostream>
-#include <climits>
-#include <string>
-#include <sstream>
-#include <vector>
 #include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <utility>
+#include <vector>
 using namespace std;
 
-vector<int> rel;
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); i++)
 
-//int dist(int pos)
-//{
-//    int d=0;
-//    for (int j=1; j<(int)rel.size(); j++)
-//        d += abs(pos-rel[j]);
-//    return d;
-//}
+multiset<int> rel;
 
 int solve()
 {
+    int best = INT_MAX;
+    int n = sz(rel);
+    int l = 0, r = n;
+    int distl = 0, distr = accumulate(all(rel), 0); 
+    int pos = 0; 
+    set<int>::iterator curr = rel.begin();
+
+    while (curr != rel.end()) {
+        pos++;
+        distr -= r;
+        distl += l;
+
+        while (curr != rel.end() && pos == *curr) {
+            l++;
+            r--;
+            curr++;
+        } 
+
+        best = min(best, distl + distr);
+    }
+
+    return best;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    cin.ignore();
-
-    while (n--) {
-        string line;
-        int d;
-        getline(cin, line);
-        stringstream ss(line);
-
+    int num;
+    cin >> num;
+    while (num--) {
+        int n;
+        cin >> n;
         rel.clear();
-        while (ss >> d)
-            rel.push_back(d);
+        while (n--) {
+            int pos;
+            cin >> pos;
+            rel.insert(pos);
+        }
 
         cout << solve() << endl;
     }
-    
+
     return 0;
 }
